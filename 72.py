4e1498fc67ffe4ec935e63fbadf119a651f2596a
Leonardo Gómez <77948810+LStitch@users.noreply.github.com>
2022-03-11 18:55:12 -0800

    Add files via upload

---
+++ 72.py
@@ -0,0 +1,67 @@
+import random
+
+K1 = [ ]
+for i in range (0 , 50):
+    K1.append(random.randint(1, 55))
+K2 = [ ]
+for i in range (0 , 50):
+    K2.append(random.randint(1, 55))
+K3 = [ ]
+for i in range (0 , 50):
+    K3.append(random.randint(1, 55))
+K4 = [ ]
+for i in range (0 , 50):
+    K4.append(random.randint(1, 55))
+K5 = [ ]
+for i in range (0 , 50):
+    K5.append(random.randint(1, 55))
+K6 = [ ]
+for i in range (0 , 50):
+    K6.append(random.randint(1, 55))
+K7 = [ ]
+for i in range (0 , 50):
+    K7.append(random.randint(1, 55))
+K8 = [ ]
+for i in range (0 , 50):
+    K8.append(random.randint(1, 55))
+K9 = [ ]
+for i in range (0 , 50):
+    K9.append(random.randint(1, 55))
+K10 = [ ]
+for i in range (0 , 50):
+    K10.append(random.randint(1, 55))
+K11 = [ ]
+for i in range (0 , 50):
+    K11.append(random.randint(1, 55))
+K12 = [ ]
+for i in range (0 , 50):
+    K12.append(random.randint(1, 55))
+K13 = [ ]
+for i in range (0 , 50):
+    K13.append(random.randint(1, 55))
+K14 = [ ]
+for i in range (0 , 50):
+    K14.append(random.randint(1, 55))
+K15 = [ ]
+for i in range (0 , 50):
+    K15.append(random.randint(1, 55))
+K16 = [ ]
+for i in range (0 , 50):
+    K16.append(random.randint(1, 55))
+K17 = [ ]
+for i in range (0 , 50):
+    K17.append(random.randint(1, 55))
+K18 = [ ]
+for i in range (0 , 50):
+    K18.append(random.randint(1, 55))
+K19 = [ ]
+for i in range (0 , 50):
+    K19.append(random.randint(1, 55))
+K20 = [ ]
+for i in range (0 , 50):
+    K20.append(random.randint(1, 55))
+
+print(K10)
+print("Primer conjunto de 10, el número mayor es: ", (max(K10)))
+print(K20)
+print("Segundo conjunto de 10, el número mayor es: ", (max(K20)))
